749735dfdbc1e4610f6046d07b5fd46bd1f253f7
Avulpe <ancavulpe@google.com>
2017-08-22 15:20:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
  * @param {Object} res Cloud Function response context.
  */
 
-exports.helloHttp1 = function helloHttp1 (req, res) {
+exports.helloHttp2 = function helloHttp2 (req, res) {
   res.send(`Hello ${req.body.name || 'World'}! :-P `);
 };
